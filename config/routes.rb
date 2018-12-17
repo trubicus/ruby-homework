be396822a2c922212236e00c384a228e7fca1434
Ivan Trubic <itrubic.truba@gmail.com>
2018-12-17 18:40:08 +0100

    assignment 1

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :posts
   resources :authors
   get 'landing/index'
+  get 'polynomials/new', to: 'polynomials#new'
+  get 'polynomials/:id', to: 'polynomials#show'
+  post 'polynomials', to: 'polynomials#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
 end
